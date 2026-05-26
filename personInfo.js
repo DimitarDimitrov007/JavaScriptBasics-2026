547e7703f513e46b2103a178bd5a9e2c18f2cdf4
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2026-05-26 09:38:12 +0300

    Delete personInfo.js

--- personInfo.js
+++
@@ -1,7 +0,0 @@
-function personInfo(firstName,secondName,age,town){
-   
-
-    console.log(`You are ${firstName} ${secondName}, a ${age}-years old person from ${town}.`)
-
-}
-personInfo("Dimitar" , "Dimitrov", 26 , "Varna");
