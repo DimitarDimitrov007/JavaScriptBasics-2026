cacfe697b1f5453963f56e16ecd22c566407cc63
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2026-05-26 09:37:12 +0300

    Delete fishTank.js

--- fishTank.js
+++
@@ -1,9 +0,0 @@
-function fishTank(length,width,height,percent){
-    let volumeInCm = length * width * height;
-    let volumeInDm3 = volumeInCm / 1000;
-    let water = volumeInDm3 - (volumeInDm3 * 0.17);
-    console.log(water);
-
-
-}
-fishTank(85,75,47,17);
