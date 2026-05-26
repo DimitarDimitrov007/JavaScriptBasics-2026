ec3ee0ad769081fb5a115db3f7c94915146cd0c6
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2026-05-26 09:38:28 +0300

    Delete projectMaster.js

--- projectMaster.js
+++
@@ -1,7 +0,0 @@
-function projectMaster(name,projects){
-    let timeForSingleProject = 3;
-    let totalTime = Number(projects) * timeForSingleProject
-    console.log(`The architect ${name} will need ${totalTime} hours to complete ${projects} project/s.`);
-
-}
-projectMaster("George",4);
