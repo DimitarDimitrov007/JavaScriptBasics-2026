04cd101bcbcc3f07b9df11520112c28b565d73e2
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2026-05-26 09:38:20 +0300

    Delete petShop.js

--- petShop.js
+++
@@ -1,8 +0,0 @@
-function petShop(dogPackets,catPackets){
-    let totalDogFoodPrice = dogPackets * 2.50 ;
-    let totalCatFoodPrice = catPackets * 4;
-    let totalSum = totalCatFoodPrice + totalDogFoodPrice;
-    console.log(`${totalSum} lv.`);
-
-}
-petShop(5,4);
