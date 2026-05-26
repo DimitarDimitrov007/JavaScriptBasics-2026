fc61fed1938eb730307edf6ee8727192c7f19912
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2026-05-26 09:36:54 +0300

    Delete bookList.js

--- bookList.js
+++
@@ -1,7 +0,0 @@
-function bookList(totalPages,pagesPerH,days){
-    let hoursNeeded = totalPages / pagesPerH;
-    let result = hoursNeeded / days;
-    console.log(result);
-
-}
-bookList(212,20,2 );
