eccc1bda70faa45c63d7437674ac74f12ed5c47b
Dimitar Dimitrov <117978668+DimitarDimitrov007@users.noreply.github.com>
2026-06-01 14:43:21 +0300

    Add files via upload

---
+++ Lesson-3-Extra/multiplyBy2.js
@@ -0,0 +1,15 @@
+function solve(input){
+
+    for(let i = 0 ; i < input.length; i++){
+        if(Number(input[i]) < 0){
+            console.log("Negative number!" );
+
+        }else{
+            console.log(`Result: ${(Number(input[i]) * 2).toFixed(2)}`)
+        }
+    }
+
+
+
+}
+solve((["12","43.2144","12.3","543.23","-20"]));
